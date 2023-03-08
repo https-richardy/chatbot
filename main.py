8f746439642b104cae2c414cbc5e55d5ead8cdb2
https-richardy <125691152+https-richardy@users.noreply.github.com>
2023-03-08 13:38:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,17 +9,12 @@ chat_bot = ChatBot()
 chat_bot.training()
 training_converse = list()
 old_data = list()
-palpites_maior = ['Que porra de chute é esse? calma ai neymar, menos que isso...', 'Passou longe, é menos que isso...', 'calma vida tá nervoso? menos...', 'kkkkk você é foda hein, eu disse menor...', 'tô me cansando disso kkkkkk']
-palpites_menor = ['Um pouco maior...', 'Quase lá, é um pouco maior, tenta de novo...', 'Tô me cansando disso kkkkk...', 'é maior q sua fimose', 'tipo a profundidade da bct da sua mae só q um pouco maior']
-
 
 while True:
     chat_bot.get_response()
 
     if chat_bot.ask == 'exit':
         break
-    if chat_bot.ask == 'adivinhar' or chat_bot.ask == 'adivinha':
-        chat_bot.game_adivinhar()
     elif chat_bot.ask not in chat_bot.training_data:
         if chat_bot.match > 90:
             continue
